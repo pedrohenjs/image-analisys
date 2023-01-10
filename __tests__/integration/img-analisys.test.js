757e0206c0067ae5d6009ee00628f80c3a3de728
pedrohenjs <pedrojs.contato@gmail.com>
2023-01-09 21:02:48 -0300

    ensure user recieves translated image rekognition text

--- __tests__/integration/img-analisys.test.js
+++ __tests__/integration/img-analisys.test.js
@@ -1,22 +1,45 @@
-const { describe, test, expect } = require("@jest/globals");
+const aws = require("aws-sdk");
 
+aws.config.update({
+  region: "us-east-1",
+});
+
+const { describe, test, expect } = require("@jest/globals");
 const requestMock = require("./../mocks/request.json");
 const { main } = require("../../src");
 
 describe("Image analyser test suite", () => {
-  test(
-    "it should analyse successfuly yhe image return the results",
-    async () => {
-      const expected = {
-        statusCode: 200,
-        body: "",
-      };
+    
+  test("it should analyse successfuly yhe image return the results", async () => {
+    const expected = {
+      statusCode: 200,
+      body: "Pastor alemão; Canino; Animal de estimação",
+    };
 
-      const result = await main(requestMock);
+    const result = await main(requestMock);
 
-      expect(result).toStrictEqual(expected);
-    }
-  );
-  test.todo("given an empty queryString it should return status code 400");
-  test.todo("given an invalid url it should return status code 500");
+    expect(result).toStrictEqual(expected);
+  });
+  test("given an empty queryString it should return status code 400", async () => {
+    const expected = {
+      statusCode: 400,
+      body: "An image URL is required",
+    };
+    const result = await main({
+      queryStringParameters: {},
+    });
+    expect(result).toStrictEqual(expected);
+  });
+  test("given an invalid url it should return status code 500", async () => {
+    const expected = {
+      statusCode: 500,
+      body: "Internal server error!",
+    };
+    const result = await main({
+      queryStringParameters: {
+        imageUrl: 3422,
+      },
+    });
+    expect(result).toStrictEqual(expected)
+  });
 });
--- src/handler.js
+++ src/handler.js
@@ -1,15 +1,82 @@
+const { get } = require("axios");
+
 module.exports = class Handler {
   constructor({ rekoSvc, translatorSvc }) {
     this.rekoSvc = rekoSvc;
     this.translatorSvc = translatorSvc;
   }
 
-  async main(event) {
-    console.log("Event: ", event);
+  async getImageBuffer(imageUrl) {
+    const response = await get(imageUrl, {
+      responseType: "arraybuffer",
+    });
 
-    return {
+    const buffer = Buffer.from(response.data, "base64");
+
+    return buffer;
+  }
+  async detectImageLabels(buffer) {
+    const result = await this.rekoSvc
+      .detectLabels({
+        Image: {
+          Bytes: buffer,
+        },
+      })
+      .promise();
+
+    const workingItems = result.Labels.filter(
+      ({ Confidence }) => Confidence > 80
+    );
+
+    const names = workingItems
+      .map(({ Name }) => Name)
+      .slice(0, 3)
+      .join("; ");
+    console.log(JSON.stringify({ names }, null, 2));
+
+    return names;
+  }
+  async translateText(text) {
+    const params = {
+      SourceLanguageCode: "en",
+      TargetLanguageCode: "pt",
+      Text: text,
+    };
+
+    const { TranslatedText } = await this.translatorSvc
+      .translateText(params)
+      .promise();
+
+    return TranslatedText;
+  }
+  async main(event) {
+    try {
+      const imageUrl = event.queryStringParameters?.imageUrl;
+      if (!imageUrl) {
+        return {
+          statusCode: 400,
+          body: "An image URL is required",
+        };
+      }
+      console.log("Downloading image...");
+      const buffer = await this.getImageBuffer(imageUrl);
+
+      console.log("Detecting labels...");
+      const response = await this.detectImageLabels(buffer);
+
+      console.log("Translating text...");
+      const texts = await this.translateText(response);
+
+      return {
         statusCode: 200,
-        body: 'Hello!'
+        body: texts,
+      };
+    } catch (error) {
+      console.log("deu ruim aq", error);
+      return {
+        statusCode: 500,
+        body: "Internal server error!",
+      };
     }
   }
 };
